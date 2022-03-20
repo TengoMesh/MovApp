7b60be2379a2ea171a2a14a173032fec9b93e72f
Tengo <meshvelianiTengo@gmail.com>
2022-03-21 03:12:52 +0400

    Added connectivity indicator  * listening to connectivity change  * updating UI when connection status is changed

--- app/src/main/java/com/example/movapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/movapp/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.movapp.ui
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -14,12 +19,31 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var uConnectivityDisplayTextView: AppCompatTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupViewPager()
+        setupConnectivityChange()
+    }
+
+    private fun setupConnectivityChange() {
+        uConnectivityDisplayTextView = findViewById(R.id.uNoConnectivityDisplay)
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                runOnUiThread {
+                    uConnectivityDisplayTextView.visibility = View.GONE
+                }
+            }
+            override fun onLost(network: Network) {
+                runOnUiThread {
+                    uConnectivityDisplayTextView.visibility = View.VISIBLE
+                }
+            }
+        })
     }
 
     private fun setupViewPager(){
