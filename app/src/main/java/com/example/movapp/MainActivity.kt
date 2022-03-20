4fd168b7c4f88fabce32b3a76af8ca6cd19b8411
Tengo <meshvelianiTengo@gmail.com>
2022-03-20 14:28:58 +0400

    Added view pager  * viewpager was added on main activity  * setup for page transform and tablayout

--- app/src/main/java/com/example/movapp/MainActivity.kt
+++ app/src/main/java/com/example/movapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.movapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager2>(R.id.uViewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.uTabLayout)
+
+        viewPager.adapter = MoviePagerAdapter(this)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
+
+    }
+}
+
+class MoviePagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity){
+    override fun getItemCount() = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return MovieListFragment.newInstance()
     }
 }
---
+++ app/src/main/java/com/example/movapp/MovieListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.movapp
+
+import androidx.lifecycle.ViewModel
+
+class MovieListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
